cb839805018b8efd5a84fd1379d043a66c526319
darkking007-bit <80806602+darkking007-bit@users.noreply.github.com>
2021-03-25 20:58:07 +0530

    Update sample_config.py

--- Sibyl_System/sample_config.py
+++ Sibyl_System/sample_config.py
@@ -3,7 +3,7 @@ from typing import Union, Optional
 API_ID: int = 12345
 API_HASH: str = "XYZ"
 STRING_SESSION: str = "XYZ:XYZ:XYZ...."
-Charlie_logs: Union[str, int] = 123456789
-Charlie_approved_logs: Union[str, int] = 123456789
+Sibyl_logs: Union[str, int] = 123456789
+Sibyl_approved_logs: Union[str, int] = 123456789
 GBAN_MSG_LOGS: Optional[Union[str, int]] = None
 BOT_TOKEN: str = "123456:shsiajskzkxhxj"
