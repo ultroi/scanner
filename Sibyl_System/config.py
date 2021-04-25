fbaacfcfca120895654f0dbebd81ae3524446880
zeuslord224 <79161058+zeuslord224@users.noreply.github.com>
2021-04-25 17:39:41 +0530

    Update config.py

--- Sibyl_System/config.py
+++ Sibyl_System/config.py
@@ -9,4 +9,5 @@ GBAN_MSG_LOGS: Optional[Union[str, int]] = None
 BOT_TOKEN: str = "123456:shsiajskzkxhxj"
 ENFORCERS: int = 123488
 INSPECTORS: int = 1993883
+MONGO_DB_URL: str = "1993838:sjsh"
 # Remove this line one you have read and understood the explanation of things in https://github.com/AnimeKaizoku/SibylSystem/blob/master/README.md
