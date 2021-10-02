b7a8fa89b5eb7e1b8cfedb0c22f1d433db08de04
ultroi <86416271+ultroi@users.noreply.github.com>
2021-10-02 16:43:30 +0530

    Create bancodes.py

---
+++ Sibyl_System/plugins/bancodes.py
@@ -0,0 +1,23 @@
+help_plus = """ Here is Help for Bancodes  :-
+֍ • {OSx00} - Scammer 
+֍ • {OSx01} - SPAM ADDING MEMBER
+֍ • {OSx02} - CHILD ABUSE
+֍ • {OSx03} - ILLEGAL
+֍ • {OSx04} - FRAUD PROMOTION  [ANY KIND]
+֍ • {OSx05} - PHISHING
+֍ • {OSx06} - BAN EVASION
+֍ • {OSx07} - RAID/SPAM INFLAMED
+֍ • {OSx08} - ADDING SPAMBOTS
+֍ • {OSx09} - KRIMINALANT
+֍ • {OSx10} - SCAMMER
+֍ • {OSx11} - ABUSE SPAM
+֍ • {OSx12} - IMPERSONATION
+֍ • {OSx13} - MD/BTC SCAM
+֍ • {OSx14} - RAID INITIALIZER
+֍ • {OSx15} - RAID PARTICIPANT
+֍ • {OSx16} - SPAMBOT
+֍ • {OSx17} - CYBER THREATENING / CYBER BULLY
+֍ • {OSx18} - NSFW SPAMMER
+[Oraizon Support](buttonurl://https://t.me/Oraizon_Support)
+"""
+__plugin_name__ = "BanCodes"
