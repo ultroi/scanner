de2fbc5734b3061604cdfe34d655b32ab4b43727
ultroi <86416271+ultroi@users.noreply.github.com>
2021-10-12 04:37:30 +0530

    Add files via upload

---
+++ Sibyl_System/strings.py
@@ -0,0 +1,64 @@
+on_string = """
+ ═══════✭✭✭✭✭════════
+ CONNECTED TO ORAIZON SYSTEM 
+═══════ ✭✭✭✭✭════════
+             「❂ USER INFO ❂」
+
+⦿ Name - {name}
+⦿ Rank  -   {enforcer} 
+
+❂Verified User ✓
+"""
+
+# Make sure not to change these too much
+# If you still wanna change it change the regex too
+scan_request_string = """
+$SCAN
+Cymatic Scan request!
+**Enforcer:** {enforcer}
+**User scanned:** {spammer}
+**Reason:** `{reason}`
+**Scan Source:** {chat}
+**Target Message:** `{message}`
+"""
+forced_scan_string = """
+$FORCED
+**Inspector:** {ins}
+**Target:** {spammer}
+**Reason:** `{reason}`
+**Scan Source:** {chat}
+**Target Message:** `{message}`
+"""
+
+reject_string = """
+$REJECTED
+**Crime Coefficient:** `Under 100`
+Not a target for enforcement action.
+The trigger of Dominator will be locked.
+"""
+
+proof_string = """
+**Case file for** - {proof_id} :
+┣━**Reason**: {reason}
+┗━**Message**
+         ┣━[Nekobin]({paste})
+         ┗━[DelDog]({url})"""
+
+scan_approved_string = """
+#LethalEliminator
+**Target User:** {scam}
+**Crime Coefficient:** `Over 300`
+**Reason:** `{reason}`
+**Enforcer:** `{enforcer}`
+**Case Number:** `{proof_id}`
+"""
+
+bot_gban_string = """
+#DestroyDecomposer
+**Enforcer:** `{enforcer}`
+**Target User:** {scam}
+**Reason:** `{reason}`
+"""
+
+# https://psychopass.fandom.com/wiki/Crime_Coefficient_(Index)
+# https://psychopass.fandom.com/wiki/The_Dominator
